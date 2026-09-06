1c37a92e606827819e27ec1372bbad6d1d1e2f87
Francesco Cassese <francescocassese9@gmail.com>
2026-09-06 18:25:15 +0200

    feat: display business details in a styled card

---
+++ src/components/CardBusiness.jsx
@@ -0,0 +1,17 @@
+import styles from './CardBusiness.module.css';
+
+function CardBusiness({ cover_image_url, name, category, address }) {
+    return (
+        <div className="card shadow-sm">
+            <div className={styles.businessCoverContainer}>
+                <img className="card-img-top" src={cover_image_url} alt={name} />
+            </div>
+            <span className={`${styles.badgeOutline} text-center`}>{category?.name}</span>
+            <div className='card-body text-center'>
+                <h3 className='card-title mb-0'>{name}<span className={styles.bullet}></span></h3>
+                <address className='mb-0 text-muted small'><i className='bi bi-geo-alt'></i> {address}</address>
+            </div>
+        </div>
+    )
+}
+export default CardBusiness

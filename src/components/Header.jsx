3cfa7c36c63091966ea830a7235c6562f7548d21
Francesco Cassese <263924667+francesco-cassese@users.noreply.github.com>
2026-09-20 22:26:05 +0200

    refactor: remove unnecessary fragments from Header and CategoryFilter

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,9 @@ import styles from './Header.module.css';
 
 function Header() {
     return (
-        <>
-            <div className={`${styles.logoSpotted} mx-auto mt-4`}>
-                <img src="/spotted_logo.png" alt="Logo-spotted" />
-            </div>
-        </>
-    )
+        <div className={`${styles.logoSpotted} mx-auto mt-4`}>
+            <img src="/spotted_logo.png" alt="Logo-spotted" />
+        </div>
+    );
 }
-export default Header
+export default Header;
